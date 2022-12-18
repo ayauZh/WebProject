5fcaa9b3078e69061052a5a6646f1e4e3b095038
Galym <210103193@stu.sdu.edu.kz>
2022-12-18 16:27:27 +0600

    dobavil comment i js

---
+++ javascript/mainPage.js
@@ -0,0 +1,11 @@
+function comments_jss(){
+    document.getElementById('comment-textarea').style.display='block';
+    document.getElementById('svernut').style.display='none';
+    document.getElementById('razvernut').style.display='block';
+}
+
+function comments_jss_2(){
+    document.getElementById('comment-textarea').style.display='none';
+    document.getElementById('svernut').style.display='block';
+    document.getElementById('razvernut').style.display='none';
+}
